31fc0433be33fd9259e4b188f39396d6b471980a
Indiver Piyush <indiverpiyush.ip@gmail.com>
2024-10-06 11:45:02 +0530

    checkout and product-detail added

---
+++ src/pages/ProductDetailPage.js
@@ -0,0 +1,11 @@
+import ProductDetail from "../features/product-list/components/ProductDetails";
+
+function ProductDetailPage() {
+  return (
+    <div>
+      <ProductDetail></ProductDetail>
+    </div>
+  );
+}
+
+export default ProductDetailPage;
